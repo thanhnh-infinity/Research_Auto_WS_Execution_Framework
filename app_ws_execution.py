2c64a6e97bcfde4d076c042e354de915e5dfe269
thanhnh-infinity <thanhnh2911@gmail.com>
2018-03-27 21:47:08 -0600

    flexible operation name

--- app_ws_execution.py
+++ app_ws_execution.py
@@ -3,11 +3,17 @@ from zeep import Client
 from pprint import pprint
 
 #print ws_execution_engine.execute_Single_Operation("./convert.wsdl","",[],[],{})
+OpenTree_Client = Client("./convert.wsdl")
+FUNCTION_LIST = {
+  'OpenTree_GetTree' : OpenTree_Client.service.ConvertSpeed_Get
+}
+result = FUNCTION_LIST['OpenTree_GetTree'](100, 'kilometersPerhour', 'milesPerhour')
+print result
+
 
 client = Client("./convert.wsdl")
 result = client.service.ConvertSpeed_Get(100, 'kilometersPerhour', 'milesPerhour')
 print result
-
 
 service2 = client.bind('ConvertSpeeds', 'ConvertSpeedsHttpGet')
 print service2.ConvertSpeed_Get(100, 'kilometersPerhour', 'milesPerhour')
