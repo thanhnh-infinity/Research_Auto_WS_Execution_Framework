00233b37741b696e811542d64ebd628d35d3c00d
thanhnh-infinity <thanhnh2911@gmail.com>
2018-06-06 04:45:29 -0600

    new thing

--- dictionary.py
+++ dictionary.py
@@ -13,22 +13,13 @@ TreeGeneration_Operation = TreeGeneration_Client.bind("phylotastic_GetPhylogenet
 SERVICE_DIC_ONTO_WSDL = {
     'phylotastic_GetPhylogeneticTree_OT_GET' : {
         'execution.call' : TreeGeneration_Operation['phylotastic_GetPhylogeneticTree_OT_GET'],
-        'inputs' : [
-            {
-                'ontology_id_1' : 'wsdl_comp_id_1'
-            },
-            {
-                'ontology_id_2' : 'wsdl_comp_id_2'
-            }
-        ],
-        'outputs' : [
-            {
-                'ontology_id_1' : 'wsdl_comp_id_1'
-            },
-            {
-                'ontology_id_2' : 'wsdl_comp_id_2'
-            }
-        ]
+        'inputs' : {
+            'resource_SetOfTaxon' : 'taxa'
+        },
+        'outputs' : {
+            'resouce_HTTPCode' : 'status_code',
+            'resource_speciesTree' : 'newick'
+        }
     },
     'ConvertSpeend_Get' : {
         'execution.call' : test_service1['ConvertSpeed_Get'],
--- ws_execution_engine.py
+++ ws_execution_engine.py
@@ -1,12 +1,19 @@
 import dictionary
+import json
 
 EX_SERVICE_DIC_ONTO_WSDL = dictionary.SERVICE_DIC_ONTO_WSDL
 
 def execute_Single_Operation(OperationName, InputParams, OutputParams):
     try:
-        result = EX_SERVICE_DIC_ONTO_WSDL['phylotastic_GetPhylogeneticTree_OT_GET']['execution.call'](InputParams)
-        #print result
-        return result
+        result = EX_SERVICE_DIC_ONTO_WSDL[OperationName]['execution.call'](InputParams)
+        
+        json_data = json.loads(result)
+        #print(json_data)
+        what_want_to_get = EX_SERVICE_DIC_ONTO_WSDL[OperationName]['outputs']['resource_speciesTree']
+        #print what_want_to_get
+        print(json_data[what_want_to_get])
+        return json_data
+
     except Exception as inst:
         print(inst)
         return None
