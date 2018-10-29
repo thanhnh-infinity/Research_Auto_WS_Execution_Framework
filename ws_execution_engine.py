2bd38f3e1ccc6ca42f7942a8e94c44fedaac9592
tayeen <abusalehmdtayeen@gmail.com>
2018-10-29 10:26:49 -0600

    added change

--- ws_execution_engine.py
+++ ws_execution_engine.py
@@ -5,7 +5,9 @@ EX_SERVICE_DIC_ONTO_WSDL = dictionary.SERVICE_DIC_ONTO_WSDL
 
 def execute_single_operation(OperationName, InputParams, OutputParams):
     try:
+        print "executing single operation.."
         result = EX_SERVICE_DIC_ONTO_WSDL[OperationName]['execution.call'](InputParams)
+        print "got result"
         json_data = json.loads(result)
         what_want_to_get = EX_SERVICE_DIC_ONTO_WSDL[OperationName]['outputs']['resource_speciesTree']
         print(json_data[what_want_to_get])
